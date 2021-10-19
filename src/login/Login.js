df1d660fb2345d0fcb35ed3028e34e60907acf43
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-10-19 17:28:14 -0400

    set skeleton

---
+++ src/login/Login.js
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+function Login() {
+  return (
+    <div className="Login">
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
+
+export default Login;
