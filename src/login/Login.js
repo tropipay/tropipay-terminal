369a18aafd11847d83b4f8fe92328466d359c38f
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-10-20 16:29:01 -0400

    update package

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,9 +1,14 @@
 import Button from "@mui/material/Button";
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
   return (
     <div className="Login">
-      <Button variant="contained">Hello World</Button>
+      <Button variant="contained" onClick={() => setCount(count + 1)}>Hello World</Button>
     </div>
   );
 }
