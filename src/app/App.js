20cb246f034494978b302165ed0cc8bf76630d71
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-10-22 17:49:56 -0400

    add routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,21 @@
-import "./App.scss";
-import './services/lang';
-import Login from "../login/Login.jsx";
-import store from './stores/AppStore';
+
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
+import "./App.scss";
+import './services/lang';
+
+import Login from "../login/Login.jsx";
+import store from './stores/AppStore';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        -
-        <Login />
+        <Switch>
+            <Route exact path='/' render={(props) => (<Login />)}/>
+            <Route path='*' exact={true} render={() => (<Redirect to="/"/>)}/>
+        </Switch>
       </div>
     </Provider>
   );
