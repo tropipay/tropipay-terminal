0346d0be53c015883484c0568d04a6a527d438a3
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-11-20 15:41:06 -0500

    add ShareThis component

--- client/src/payment/components/PaymentPage.jsx
+++ client/src/payment/components/PaymentPage.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from "react-i18next";
+import ShareThis from '../../app/components/ShareThis/ShareThis';
 
 function PaymentPage() {
   const dispatch = useDispatch();
@@ -16,6 +17,7 @@ function PaymentPage() {
   return (
     <div className="Demo">
       {t("demo.title", { test: 'payment' })}
+      <ShareThis />
     </div>
   );
 }
