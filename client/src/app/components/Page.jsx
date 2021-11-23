115f4ccb8679668e9b9484c000309c2cb033762d
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-11-22 22:00:11 -0500

    update page

--- client/src/app/components/Page.jsx
+++ client/src/app/components/Page.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useSelector } from "react-redux";
-//import { Route, Redirect } from "react-router-dom";
 import Menu from './Menu';
 
 export default function Page({ children, ...rest }) {
-    //const profile = useSelector(selectProfile);
     return (
         <div>
             <Menu />
