e1f1d8a7a911d856065d75c70ec831d23d5991fb
Antonio Membrides Espinosa <tonykssa@gmail.com>
2021-11-27 10:04:17 -0500

    add validations

---
+++ client/src/app/services/validation.js
@@ -0,0 +1,31 @@
+export default {
+    required: (t) => t("error.required"),
+    string: (t) => {
+        return {
+            value: /^[A-Za-z]+$/i,
+            message: t("error.string")
+        }
+    },
+    number: (t) => {
+        return {
+            value: /^-?\d+(?:,\d+)?(?:[Ee][-+]?\d+)?$/i,
+            message: t("error.numeric")
+        }
+    },
+    min: (t, v) => {
+        return {
+            value: v,
+            message: t("error.min", {
+                value: v
+            })
+        }
+    },
+    maxStrLength: (t, v = 100) => {
+        return {
+            value: v,
+            message: t("error.maxStr", {
+                value: v
+            })
+        }
+    },
+};
