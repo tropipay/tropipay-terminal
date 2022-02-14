0df9931124c5f0c4a5487171f56b39732ae537d8
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-02-14 08:22:35 -0500

    update dependency

--- server/src/movement/index.js
+++ server/src/movement/index.js
@@ -8,13 +8,14 @@
  * */
 const KsMf = require('ksmf');
 
-const TokenRequire = require('../security/middleware/TokenRequire');
 class MovementModule extends KsMf.app.Module {
 
     initConfig() {
         const prefix = "/api/v1" + this.prefix;
-
-        this.middleware.global.push(TokenRequire);
+        const TokenRequire = this.helper.get('middleware.token');
+        if(TokenRequire) {
+            this.middleware.global.push(TokenRequire);
+        }
         this.routes = [{
             route: prefix + "/",
             controller: 'DefaultController',
