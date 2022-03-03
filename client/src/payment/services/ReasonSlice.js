336082ebf75bf0f8eeaec23979b61657f670249b
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-03-03 11:52:15 -0300

    clean code

--- client/src/payment/services/ReasonSlice.js
+++ client/src/payment/services/ReasonSlice.js
@@ -27,11 +27,6 @@ export const slice = createSlice({
 // ... Actions  
 const { onError, onUpdate } = slice.actions;
 
-// ... Load reasons from server
-const onLoad = () => (dispatch) => {
-    httpReq("/api/v1/payment/reasons", dispatch).then(data => dispatch(onUpdate(data)));
-};
-
 //... Export the slice as a service
 const Service = {
     name,
@@ -42,7 +37,10 @@ const Service = {
     },
     action: {
         update: (data) => dependencies.dispatch(onUpdate(data)),
-        load: () => dependencies.dispatch(onLoad())
+        load: () => dependencies.dispatch((dispatch) => {
+            // ... Load reasons from server
+            httpReq("/api/v1/payment/reasons", dispatch).then(data => dispatch(onUpdate(data)));
+        })
     },
     selector: {
         data:  () => dependencies.useSelector((state) => state[name].data),
